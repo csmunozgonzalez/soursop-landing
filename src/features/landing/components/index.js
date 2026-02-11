b04729c3f535203218e2873d8c5a356383db05d3
csmunozgonzalez <cristian.stiven9809@gmail.com>
2026-02-11 13:46:40 -0500

    deploy: manual production build

--- src/features/landing/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as LandingHero } from './LandingHero';
-export { default as LandingFeatures } from './LandingFeatures';
-export { default as LandingPricing } from './LandingPricing';
-export { default as LandingDNA } from './LandingDNA';
-export { default as LandingFooter } from './LandingFooter';
-export { default as LandingNavbar } from './LandingNavbar';
-export { default as LandingBlog } from './LandingBlog';
